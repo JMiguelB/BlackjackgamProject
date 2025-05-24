c593c446b49ec871bbeda2d77230575209308916
JMiguelB <jbelarmino3@student.rccd.edu>
2025-05-24 00:00:31 -0700

    Signed-off-by: JMiguelB <jbelarmino3@student.rccd.edu>

---
+++ V1_project2/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: migue
+ *
+ * Created on May 23, 2025, 12:32 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
